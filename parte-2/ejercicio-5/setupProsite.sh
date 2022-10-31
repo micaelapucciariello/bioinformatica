1c8f91fb21208930c51379a0624cdd92e8d2f2c5
FranciscoII <francortez993@gmail.com>
2022-10-31 20:48:53 -0300

    Subo script para descargar la base de datos de prosite

---
+++ parte-2/ejercicio-5/setupProsite.sh
@@ -0,0 +1,3 @@
+wget --no-check-certificate "https://drive.google.com/uc?export=download&id=1jptJtZAod_ZxAF67pj2GkyYdWumkTU70" -O prosite.dat
+wget --no-check-certificate "https://drive.google.com/uc?export=download&id=1pzRxvR5EPLRCoUuUrrfyOGdc8Wwi1FQO" -O prosite.doc
+
